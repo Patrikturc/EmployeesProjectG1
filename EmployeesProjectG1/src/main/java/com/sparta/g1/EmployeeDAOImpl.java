5b948c29b48e8d19c313ed64f0717a048b6d14d0
Lewis McFarlane <lmcfarlane@spartaglobal.com>
2024-06-25 16:31:29 +0100

    fix: Fix typo in ArrayList name

--- EmployeesProjectG1/src/main/java/com/sparta/g1/EmployeeDAOImpl.java
+++ EmployeesProjectG1/src/main/java/com/sparta/g1/EmployeeDAOImpl.java
@@ -4,8 +4,8 @@ package com.sparta.g1;
 import java.util.ArrayList;
 
 
-public class EmployeeDAO {
-    private ArrayList<EmployeeDTO> employeeDTOS = new ArrayList<>();
+public class EmployeeDAOImpl {
+    private ArrayList<EmployeeDTO> employeeDTO = new ArrayList<>();
 
 }
 
