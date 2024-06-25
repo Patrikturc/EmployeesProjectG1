5e2ebd5b9fc678e8e00764957b015bdf80f8d955
l-hepper <liamhepper1@gmail.com>
2024-06-25 17:06:16 +0100

    FEAT: add searchByID(String id) method to EmployeeDAOImpl

--- EmployeesProjectG1/src/main/java/com/sparta/g1/EmployeeDAOImpl.java
+++ EmployeesProjectG1/src/main/java/com/sparta/g1/EmployeeDAOImpl.java
@@ -7,5 +7,16 @@ import java.util.ArrayList;
 public class EmployeeDAOImpl {
     private ArrayList<EmployeeDTO> employeeDTO = new ArrayList<>();
 
+    public EmployeeDTO searchByID(String id) {
+        EmployeeDTO foundEmployee = null;
+
+        for (EmployeeDTO employee : employeeDTO) {
+            if (employee.getID().equals(id)) {
+                foundEmployee = employee;
+            }
+        }
+
+        return foundEmployee;
+    }
 }
 
