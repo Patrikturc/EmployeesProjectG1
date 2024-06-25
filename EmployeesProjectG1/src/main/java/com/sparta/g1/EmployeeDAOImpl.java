e7eda1a2ad3ea64b11a62c1fc127e84e8407efc4
Chris Rayner <crayner@spartaglobal.com>
2024-06-25 16:54:12 +0100

    Rebased DAOChris with DAO

--- EmployeesProjectG1/src/main/java/com/sparta/g1/EmployeeDAOImpl.java
+++ EmployeesProjectG1/src/main/java/com/sparta/g1/EmployeeDAOImpl.java
@@ -4,8 +4,20 @@ package com.sparta.g1;
 import java.util.ArrayList;
 
 
+
 public class EmployeeDAOImpl {
     private ArrayList<EmployeeDTO> employeeDTO = new ArrayList<>();
 
+    public EmployeeDTO searchByLastName(String lastName){
+        for(EmployeeDTO employee: employeeDTO){
+            if(employee.getLastName()
+                    .toLowerCase()
+                    .contains(lastName.toLowerCase())){
+                return employee;
+                break;
+            }
+        }
+        return null;
+    }
 }
 
