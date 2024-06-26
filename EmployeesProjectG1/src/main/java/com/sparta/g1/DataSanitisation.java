289bd0565d9684271e1f8f10715aca5137478dde
l-hepper <liamhepper1@gmail.com>
2024-06-26 17:59:16 +0100

    fixed checkValidGenderMethod() to increase number of corrupted entries when false, also fixed comparison with String type

--- EmployeesProjectG1/src/main/java/com/sparta/g1/DataSanitisation.java
+++ EmployeesProjectG1/src/main/java/com/sparta/g1/DataSanitisation.java
@@ -5,6 +5,12 @@ public class DataSanitisation {
     private int numberOfCorruptedEntries = 0;
 
     public boolean checkValidGender(Employee employee) {
-        return employee.gender() == 'M' || employee.gender() == 'F';
+        if (employee.gender().equals("M") || employee.gender().equals("F")) {
+            return true;
+        } else {
+            numberOfCorruptedEntries++;
+            return false;
+        }
     }
+
 }
