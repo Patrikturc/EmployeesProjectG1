8015abe5aa58896b7c75b00568874eaba7ad1386
Patrik Turcani <pturcani@spartaglobal.com>
2024-06-26 08:56:01 +0100

    fixing conflicts

--- EmployeesProjectG1/src/main/java/com/sparta/g1/EmployeeDAOImpl.java
+++ EmployeesProjectG1/src/main/java/com/sparta/g1/EmployeeDAOImpl.java
@@ -5,11 +5,11 @@ import java.util.ArrayList;
 
 
 public class EmployeeDAOImpl {
-    private ArrayList<Employee> employeeDTO = new ArrayList<>();
+    private ArrayList<EmployeeDTO> employeeDTO = new ArrayList<>();
 
 
-    public Employee searchByID(String id) {
-        Employee foundEmployee = null;
+    public EmployeeDTO searchByID(String id) {
+        EmployeeDTO foundEmployee = null;
 
         for (EmployeeDTO employee : employeeDTO) {
             if (employee.getID().equals(id)) {
@@ -21,8 +21,8 @@ public class EmployeeDAOImpl {
         return foundEmployee;
     }
 
-        public Employee searchByLastName (String lastName){
-            for (Employee employee : employeeDTO) {
+        public EmployeeDTO searchByLastName (String lastName){
+            for (EmployeeDTO employee : employeeDTO) {
 
                 if (employee.getLastName()
                         .toLowerCase()
