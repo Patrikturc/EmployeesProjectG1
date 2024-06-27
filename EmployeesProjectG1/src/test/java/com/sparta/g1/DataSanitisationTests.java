7adb36b3ce35a2228600ec551f7b977a863ccdcb
Patrik Turcani <pturcani@spartaglobal.com>
2024-06-27 18:34:43 +0100

    Fixed a bug with instantiating the test employee objects

--- EmployeesProjectG1/src/test/java/com/sparta/g1/DataSanitisationTests.java
+++ EmployeesProjectG1/src/test/java/com/sparta/g1/DataSanitisationTests.java
@@ -24,13 +24,13 @@ public class DataSanitisationTests {
         eric = new Employee("423093", "Mr.", "Eric", "O", "Manning", "M", "eric.manning@yahoo.com", LocalDate.of(1980,10,20), LocalDate.of(2002,11,2), "149363" );
         Renetta = new Employee("207808", "Ms.", "Renetta", "T", "Hafner", "F", "renetta.hafner@aol.com", LocalDate.of(1975,10,20), LocalDate.of(1998,11,2), "189363" );
         // Incorrect gender
-        Employee john = new Employee("123456", "Mr.", "John", "D", "Doe", "X", "john.doe@gmail.com", LocalDate.of(1980,1,1), LocalDate.of(2000,1,1), "123456");
+        john = new Employee("123456", "Mr.", "John", "D", "Doe", "X", "john.doe@gmail.com", LocalDate.of(1980,1,1), LocalDate.of(2000,1,1), "123456");
         // Incorrect email
-        Employee jane = new Employee("654321", "Ms.", "Jane", "D", "Doe", "F", "jane.doegmail.com", LocalDate.of(1985,1,1), LocalDate.of(2005,1,1), "654321");
+        jane = new Employee("654321", "Ms.", "Jane", "D", "Doe", "F", "jane.doegmail.com", LocalDate.of(1985,1,1), LocalDate.of(2005,1,1), "654321");
         // Incorrect employee ID length
-        Employee bob = new Employee("7890", "Mr.", "Bob", "B", "Builder", "M", "bob.builder@gmail.com", LocalDate.of(1970,1,1), LocalDate.of(1990,1,1), "789012");
+        bob = new Employee("7890", "Mr.", "Bob", "B", "Builder", "M", "bob.builder@gmail.com", LocalDate.of(1970,1,1), LocalDate.of(1990,1,1), "789012");
         // Date of birth is not before date of joining
-        Employee alice = new Employee("987654", "Ms.", "Alice", "A", "Adams", "F", "alice.adams@gmail.com", LocalDate.of(2000,1,1), LocalDate.of(1990,1,1), "987654");
+        alice = new Employee("987654", "Ms.", "Alice", "A", "Adams", "F", "alice.adams@gmail.com", LocalDate.of(2000,1,1), LocalDate.of(1990,1,1), "987654");
 
     }
 
