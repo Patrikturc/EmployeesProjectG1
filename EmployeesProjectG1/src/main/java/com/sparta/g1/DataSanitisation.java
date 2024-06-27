99ac1799c6532d6f76095f5f3000e61e2c84a349
KianDoyleSparta <kdoyle@spartaglobal.com>
2024-06-27 09:21:56 +0100

    Updated DataSanitisation file

--- EmployeesProjectG1/src/main/java/com/sparta/g1/DataSanitisation.java
+++ EmployeesProjectG1/src/main/java/com/sparta/g1/DataSanitisation.java
@@ -1,19 +1,53 @@
 package com.sparta.g1;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class DataSanitisation {
 
-    private int numberOfCorruptedEntries = 0;
+    private static int numberOfCorruptedEntries = 0;
 
-    public boolean checkValidGender(Employee employee) {
-        if (employee.gender().equals("M") || employee.gender().equals("F")) {
+    public static boolean checkValidGender(String gender) {
+        if (gender.equals("M") || gender.equals("F")) {
             return true;
         } else {
             numberOfCorruptedEntries++;
             return false;
         }
+    }
+    public static boolean isValidEmail (String email){
+        if (email.matches("^([a-zA-Z0-9_\\-.]+)@([a-zA-Z0-9_\\-.]+)\\.([a-zA-Z]{2,5})$")){
+            return true;
+        }else{
+            numberOfCorruptedEntries++;
+            return false;
+        }
+    }
 
-    public static boolean isValidEmail(Employee employee) {
-        return employee.email().matches("^([a-zA-Z0-9_\\-.]+)@([a-zA-Z0-9_\\-.]+)\\.([a-zA-Z]{2,5})$");
+    public static boolean checkEmpIdIsCorrectLength (String empId){
+        if (empId.length() == 6) {
+            return true;
+        } else {
+            numberOfCorruptedEntries++;
+            return false;
+        }
+    }
+
+    public static boolean isValidSalary(String salary){
+        if(Integer.parseInt(salary)<0){
+            numberOfCorruptedEntries++;
+            return false;
+        }
+        return true;
+    }
+
+    public static int getNumberOfCorruptedEntries () {
+        return numberOfCorruptedEntries;
+    }
+
+    public static DateTimeFormatter formatDates () {
+        return DateTimeFormatter.ofPattern("[MM/dd/yyyy][M/d/yyyy][M/dd/yyyy][M/d/yyyy]");
     }
 
 }
+
