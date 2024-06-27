028d64a5b17f5aa04f464a1ca8cc68864c68f966
Chris Rayner <crayner@spartaglobal.com>
2024-06-27 21:18:05 +0100

    init hasValidDates method in FieldChecks

--- EmployeesProjectG1/src/main/java/com/sparta/g1/FieldChecks.java
+++ EmployeesProjectG1/src/main/java/com/sparta/g1/FieldChecks.java
@@ -1,5 +1,15 @@
 package com.sparta.g1;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.time.format.DateTimeParseException;
+import java.time.format.SignStyle;
+import java.time.temporal.ChronoField;
+import java.util.Date;
+
 public class FieldChecks {
 
     public static boolean hasValidID(String empID){
@@ -23,7 +33,24 @@ public class FieldChecks {
     }
 
     public static boolean hasValidDates(String date1, String date2){
-
+        if(date1.length() <8 || date2.length() <8 || date1.length()>10 || date2.length() >10){
+            return false;
+        }
+        DateTimeFormatter formatter = new DateTimeFormatterBuilder()
+                .appendValue(ChronoField.MONTH_OF_YEAR,1,2, SignStyle.NEVER)
+                .appendLiteral('/')
+                .appendValue(ChronoField.DAY_OF_MONTH,1,2,SignStyle.NEVER)
+                .appendLiteral('/')
+                .appendValue(ChronoField.YEAR,4)
+                .toFormatter();
+        try{
+            LocalDate.parse(date1, formatter);
+            LocalDate.parse(date2, formatter);
+            return true;
+        }
+        catch (DateTimeParseException e){
+            return false;
+        }
     }
 
     public static boolean hasValidAgeRange(String age1, String age2){
