cfd04499a83a14787a92a9f04dd617196bc47497
Lewis McFarlane <lmcfarlane@spartaglobal.com>
2024-06-28 02:19:48 +0100

    refactor: Abstract data validation checks from createEmployeeData method

--- EmployeesProjectG1/src/main/java/com/sparta/g1/EmployeeDataConverter.java
+++ EmployeesProjectG1/src/main/java/com/sparta/g1/EmployeeDataConverter.java
@@ -23,32 +23,31 @@ public class EmployeeDataConverter {
     private static Employee createEmployeeFromData(String data) {
         String[] parts = data.split(",");
         if (parts.length != 10) throw new IllegalArgumentException("Invalid data format");
-        else if(!DataSanitisation.checkEmpIdIsCorrectLength(String.valueOf(Integer.parseInt(parts[0])))){
+        if (!isValidEmployeeDate(parts)) {
             return null;
         }
-        else if(!DataSanitisation.checkValidGender(String.valueOf(parts[5].charAt(0)))){
-            return null;
-        }
-        else if(!DataSanitisation.isValidEmail(parts[6])){
-            return null;
-        }
-        else if(!DataSanitisation.isValidSalary(parts[9])){
-            return null;
-        }
-        else {
-            String empID = String.valueOf(Integer.parseInt(parts[0]));
-            String prefix = parts[1];
-            String firstName = parts[2];
-            String middleInitial = String.valueOf(parts[3].charAt(0));
-            String lastName = parts[4];
-            String gender = String.valueOf(parts[5].charAt(0));
-            String email = parts[6];
-            LocalDate dateOfBirth = LocalDate.parse(parts[7], DataSanitisation.formatDates());
-            LocalDate dateOfJoining = LocalDate.parse(parts[8], DataSanitisation.formatDates());
-            String salary = String.valueOf(Integer.parseInt(parts[9]));
+        String empID = String.valueOf(Integer.parseInt(parts[0]));
+        String prefix = parts[1];
+        String firstName = parts[2];
+        String middleInitial = String.valueOf(parts[3].charAt(0));
+        String lastName = parts[4];
+        String gender = String.valueOf(parts[5].charAt(0));
+        String email = parts[6];
+        LocalDate dateOfBirth = LocalDate.parse(parts[7], DataSanitisation.formatDates());
+        LocalDate dateOfJoining = LocalDate.parse(parts[8], DataSanitisation.formatDates());
+        String salary = String.valueOf(Integer.parseInt(parts[9]));
 
-            return new Employee(empID, prefix, firstName, middleInitial, lastName, gender, email, dateOfBirth, dateOfJoining, salary);
-        }
+        return new Employee(empID, prefix, firstName, middleInitial, lastName, gender, email, dateOfBirth, dateOfJoining, salary);
+    }
+
+
+    private static boolean isValidEmployeeDate(String[] parts) {
+        return DataSanitisation.checkEmpIdIsCorrectLength(String.valueOf(Integer.parseInt(parts[0]))) &&
+                DataSanitisation.checkValidGender(String.valueOf(parts[5].charAt(0))) &&
+                DataSanitisation.isValidEmail(parts[6]) &&
+                DataSanitisation.isDateOfBirthValid(parts[7]) &&
+                DataSanitisation.isDateOfJoiningValid(parts[8]) &&
+                DataSanitisation.isValidSalary(parts[9]);
     }
 
     public static HashSet<Employee> getListOfEmployees() throws IOException {
