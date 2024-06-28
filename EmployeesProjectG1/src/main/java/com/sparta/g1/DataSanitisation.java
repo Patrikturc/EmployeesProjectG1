bd21d676d5ddd3529a2b4a5c601ad774472219e1
Lewis McFarlane <lmcfarlane@spartaglobal.com>
2024-06-28 02:16:51 +0100

    feat: Enhance date validation by checking valid days of month and leap years

--- EmployeesProjectG1/src/main/java/com/sparta/g1/DataSanitisation.java
+++ EmployeesProjectG1/src/main/java/com/sparta/g1/DataSanitisation.java
@@ -12,6 +12,17 @@ public class DataSanitisation {
 
     private static int numberOfCorruptedEntries = 0;
 
+    //1. isValidID
+    //2. isValidPrefix
+    //3. valid first name?
+    //4. valid middle initial
+    //5. valid last name
+    //6. isValidGender
+    //7. isValidEmail
+    //8. isValidDoB
+    //9. isValidDoJ
+    //10. isValidSalary
+
     public static boolean checkValidGender(String gender) {
         if (gender.equals("M") || gender.equals("F")) {
             return true;
@@ -71,6 +82,29 @@ public class DataSanitisation {
         return DateTimeFormatter.ofPattern("[MM/dd/yyyy][M/d/yyyy][M/dd/yyyy][M/d/yyyy]");
     }
 
+
+    private static boolean isValidDayOfMonth(int month, int day, int year) {
+        if (((month == 4 || month == 6 || month == 9
+                || month == 11) && day > 30)) {
+            return false;
+        }
+        if (month == 2) {
+            if (isLeapYear(year) && day > 29) {
+                return false;
+            } else if (!isLeapYear(year) && day > 28) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static boolean isLeapYear(int year) {
+        return (year % 4 == 0 && year % 100 != 0 || year % 400 == 0);
+    }
+
+
+
+
     public static boolean isDateOfBirthValid(String dateOfBirth) {
         String[] parts = dateOfBirth.split("/");
         if (parts.length != 3) {
@@ -82,17 +116,18 @@ public class DataSanitisation {
             int day = Integer.parseInt(parts[1]);
             int year = Integer.parseInt(parts[2]);
 
-            if (month < 1 || month > 12) {
+            if (month < 1 || month > 12 ||
+                    day < 1 || day > 31 ||
+                    year < 1924 || year > LocalDate.now().getYear()) {
                 numberOfCorruptedEntries++;
                 return false;
             }
-            if (day < 1 || day > 31) {
+            if (!isValidDayOfMonth(month, day, year)) {
                 numberOfCorruptedEntries++;
                 return false;
             }
-            if (year >= 1924 && year <= LocalDate.now().getYear()) {
-                return true;
-            }
+            return true;
+
         } catch (NumberFormatException | DateTimeParseException e) {
             numberOfCorruptedEntries++;
         }
@@ -110,22 +145,26 @@ public class DataSanitisation {
             int day = Integer.parseInt(parts[1]);
             int year = Integer.parseInt(parts[2]);
 
-            if (month < 1 || month > 12) {
+            if (month < 1 || month > 12 ||
+                    day < 1 || day > 31 ||
+                    year < 1979 || year > LocalDate.now().getYear()) {
                 numberOfCorruptedEntries++;
                 return false;
             }
-            if (day < 1 || day > 31) {
+            if (!isValidDayOfMonth(month, day, year)) {
                 numberOfCorruptedEntries++;
                 return false;
             }
-            if (year >= 1979 && year <= LocalDate.now().getYear()) {
-                return true;
-            }
+            return true;
+
         } catch (NumberFormatException | DateTimeParseException e) {
             numberOfCorruptedEntries++;
         }
         return false;
     }
+
+
+
 
 
 
